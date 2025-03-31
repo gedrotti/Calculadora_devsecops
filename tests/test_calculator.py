b86ffce1bd9ce6ac784cdb7d9deaeead7f4d0fa0
germanopedrotti <germanopedrotti@hotmail.com>
2025-03-31 17:31:24 -0400

    🐛 Corrige import de calculator para app.calculator nos testes

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -5,7 +5,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 print(f" Adicionando ao sys.path: {project_root}")
 sys.path.insert(0, project_root)
 
-from calculator import add, substract
+from app.calculator import add, subtract
 
 def test_add():
     assert add(3, 2) == 5
