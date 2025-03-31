31b6defe866cc1a9023ad6c1df1bcaf0523c3b74
germanopedrotti <germanopedrotti@hotmail.com>
2025-03-31 16:19:13 -0400

    Corrige import para execução direta via pytest no GitHub Actions

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,13 @@
-from app.calculator import add, subtract
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+print(f" Adicionando ao sys.path: {project_root}")
+sys.path.insert(0, project_root)
+
+#from app.calculator import add, subtract
+
+from calculator import add, substract
 
 def test_add():
     assert add(3, 2) == 5
